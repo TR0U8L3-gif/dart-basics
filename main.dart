22248b1ec45fed4e81e505e6db2109dcefb80cab
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2023-06-04 15:22:25 +0200

    Initialization with Constructors

--- main.dart
+++ main.dart
@@ -5,9 +5,26 @@ int main(){
 
 class Deck {
   List<Card> cards = [];
+
+  Deck(){
+    List<Card> cards = [];
+    var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+  	var suits = ['D', 'H', 'C', 'S'];
+  
+  	for(var rank in ranks){
+      for(var suit in suits){
+        Card card = Card(rank: rank, suit: suit);
+      	cards.add(card);
+      }
+    }
+    cards.shuffle();
+    this.cards = cards;
+  }
 }
 
 class Card {
   String suit;
   String rank;
+
+  Card({required this.suit, required this.rank});
 }
